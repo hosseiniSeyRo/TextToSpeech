289f095b10db6976e9d41343b46701a8dc277f58
zoho1381 <zoho1381@gmail.com>
2018-11-11 21:48:39 +0330

    2- Add main codes

--- app/src/main/java/com/parsdroid/texttospeech/MainActivity.java
+++ app/src/main/java/com/parsdroid/texttospeech/MainActivity.java
@@ -1,13 +1,76 @@
 package com.parsdroid.texttospeech;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+/*
+ * Developed by R.hosseini
+ * 11/4/2018
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TTS_TAG = "TTS";
+    EditText mEditText;
+    Button mButtonSpeak;
+    TextToSpeech mTTS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.editText);
+        mButtonSpeak = findViewById(R.id.buttonSpeak);
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.ERROR) {
+                    Log.e(TTS_TAG, "Initialize failed");
+                } else {
+                    int result = mTTS.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_NOT_SUPPORTED
+                            || result == TextToSpeech.LANG_MISSING_DATA) {
+                        Log.e(TTS_TAG, "Language not supported");
+                    }
+                }
+            }
+        });
+
+        mButtonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        String text = mEditText.getText().toString();
+        if (text.length() == 0) {
+            Toast.makeText(this, "text is empty", Toast.LENGTH_SHORT).show();
+        } else {
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
     }
 }
